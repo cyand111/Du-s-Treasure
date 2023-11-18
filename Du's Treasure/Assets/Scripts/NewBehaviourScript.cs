151492e750eaa26156124dabee9a2e7a9dabb0ea
EDuDu <1838384280@qq.com>
2023-11-18 23:57:07 +0800

    tong first commit

---
+++ Du's Treasure/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("a");
+    }
+
+    void FixedUpdate(){
+        transform.position = Vector3.zero;            
+    }   
+     
+}
